7c3871b4b62a5f76e1edc536b5d85e5f2752d979
ZiaSandhu <ziar4556@gmail.com>
2024-06-01 22:46:47 +0500

    add some changes

--- controllers/ranking.py
+++ controllers/ranking.py
@@ -3,21 +3,44 @@ from collections import defaultdict
 from flask import jsonify
 import random
 
+# from gensim.summarization import summarize
+# from gensim.sumarization import keywords
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
+from sumy.parsers.plaintext import PlaintextParser
+from sumy.nlp.tokenizers import Tokenizer
+from sumy.summarizers.lsa import LsaSummarizer
+
+def summarize(text,ratio=1):
+    parser = PlaintextParser.from_string(text, Tokenizer("english"))
+    summarizer = LsaSummarizer()
+    summary = summarizer(parser.document,ratio)  # Summarize to 3 sentences
+    return " ".join(str(sentence) for sentence in summary)
+
+
 def ranking(request):
 
     if 'file' not in request.files:
         return "Files not found", 409
     files = request.files.getlist('file')
     data = list()
+    jd = request.form.get('jobDescription')
+    summarize_description = summarize(jd,ratio=1)
+    print("description",summarize_description)
 
     for file in files:
-        # print(f"Processing file at index {index}: {file}")
-        # extractedText = extract_text_from_pdf(file)
-        # label = classify(extractedText)
+        extractedText = extract_text_from_pdf(file)
 
-        data.append(file.filename)
+        textlist = [summarize_description,extractedText] 
+        cv = CountVectorizer()
+        countmatrix = cv.fit_transform(textlist)
+        matchpercentage = cosine_similarity(countmatrix)[0][1]*100
+        matchpercentage = round(matchpercentage,2)
+        data.append({"name":file.filename,"score":matchpercentage})
 
-    random.shuffle(data)
+    # print(data)
+
     response_Data = {
                     "message":"Ranking Resume",
                     "success":"true",
--- utils/extractText.py
+++ utils/extractText.py
@@ -1,9 +1,31 @@
 import PyPDF2
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+import re
 
 def extract_text_from_pdf(file):
-    text = ""
+    raw = ""
     reader = PyPDF2.PdfReader(file)
     for page_num in range(len(reader.pages)):
         page = reader.pages[page_num]
-        text += page.extract_text()
-    return text
+        raw += page.extract_text()
+    
+    return pre_process_text(raw)
+
+def remove_stopwords(text):
+    stop_words = set(stopwords.words('english'))
+    stop_words.add('http')
+    filtered_text = [word for word in text if word.lower() not in stop_words]
+    return ' '.join(filtered_text)
+
+def remove_punctuation(text):
+    return re.sub(r'[^\w\s]', '', text)
+
+def pre_process_text(raw):
+    # Remove punctuation
+    text_no_punc = remove_punctuation(raw)
+    # Tokenize text
+    words = word_tokenize(text_no_punc)
+    # Remove stopwords
+    clean_text = remove_stopwords(words)
+    return clean_text
